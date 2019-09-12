b9815be258dc61d452b651a18a54fb6dcfea2ff4
Roland Bisasso <roland.bisasso@gmail.com>
2019-09-13 02:45:47 +0300

    modified a few files

---
+++ signup.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+
+console.log("started");
+
+$('signUpBtn').click(() => {
+
+let userFirstName = $('#firstName').val();
+let userSecondName = $('#SecondName').val();
+let userEmail = $('#userEmail').val();})	
+let userPassword = $('#userPassword').val();
+
+	console.log("First  Name =>", userFirstName);
+})
+
